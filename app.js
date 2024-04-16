904299151638dd896ff36b096986f7fcda33f618
DESKTOP-70DVC3H\Yoana <yaneva.y6@gmail.com>
2024-04-16 21:50:44 +0300

    Handaling login routes and creting unstyled login page

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3003;
-const middleware = require('./middleware')
+const middleware = require('./middleware');
 
 const server = app.listen(port, () => console.log("Server up and running at port: " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+//Routes
+const loginRoute = require('./routes/loginRoutes');
+
+app.use("/login", loginRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
---
+++ routes/loginRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+
+router.get("/", (req, res, next) => {
+    let payload = {
+        pageTitle: "Login" // DONT FORGET REGISTER PAGE
+    }
+    res.status(200).render("login", payload);
+})
+
+module.exports = router;
