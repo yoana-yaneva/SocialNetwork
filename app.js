6c0d9a6c6fe86c40e5f477153224412af07b4e66
DESKTOP-70DVC3H\Yoana <yaneva.y6@gmail.com>
2024-04-19 21:01:23 +0300

    Register page validation added

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const middleware = require('./middleware');
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const server = app.listen(port, () => console.log("Server up and running at port: " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")))
 
 //Routes
@@ -18,6 +20,7 @@ const registerRoute = require('./routes/registerRoutes');
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 router.get("/", (req, res, next) => {
 
     let payload = {
-        pageTitle: "Register" // DONT FORGET REGISTER PAGE
+        pageTitle: "Register"
     }
-    
+
     res.status(200).render("register", payload);
+})
+
+router.post("/", (req, res, next) => {
+
+    let firstName = req.body.firstName.trim();
+    let lastName = req.body.lastName.trim();
+    let email = req.body.email.trim();
+    let userName = req.body.userName.trim();
+    let password = req.body.password;
+
+    let payload = req.body;
+
+    if (firstName && lastName && userName && email && password) {
+
+    } else {
+        payload.errorMessage = "Make sure each field has a valid value."
+        res.status(200).render("register", payload);
+    }
 })
 
 module.exports = router;
